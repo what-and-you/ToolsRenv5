033f1c666b1f1be78d92933e086e31badce5c2a8
REN9999 <sejatipria990@gmail.com>
2024-08-08 21:46:47 +0700

    Update register.sh

--- register.sh
+++ register.sh
@@ -1,19 +1,10 @@
 #!/bin/bash
 
-# File untuk menyimpan informasi registrasi
 REGISTRATION_FILE="/sdcard/registration_status.txt"
 
-# Fungsi untuk melakukan registrasi
 register_user() {
   read -p "Masukkan nama: " name
   read -p "Masukkan nomor telepon: " phone_number
   echo "Nama: $name" > "$REGISTRATION_FILE"
   echo "Registrasi berhasil. Anda sekarang dapat mengakses script utama."
 }
-
-# Memeriksa apakah pengguna sudah terdaftar
-if [ -f "$REGISTRATION_FILE" ]; then
-  echo "Anda sudah terdaftar. Silakan gunakan script utama."
-else
-  register_user
-fi
